fe59b94efc752d7d214fdbb32eb3b6a58be72be0
Osvaldo Sanchez <osvaldopadillasanchez@gmail.com>
2024-08-14 23:09:45 -0400

    Nav Bar Modified

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+/*Landing Page On Load Animation*/
+$(window).on("load", function(){
+    $("#landing-page").addClass("animate");
+    $(".logo-container > *").addClass("visible");
+    $("dotlottie-player").addClass("visible");
+});
+
 var sticky = $("nav").offset().top;
 
 $(window).scroll(function(){
